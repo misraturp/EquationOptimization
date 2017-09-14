4a6c201b3ead3ad9f14ea1b9b294915d880bb517
Cesare James Carissimo <CesareCarissimo@Cesares-MacBook-Pro.local>
2017-09-14 15:59:01 +0200

    KGF

--- src/player25.java
+++ src/player25.java
@@ -16,7 +16,7 @@ public class player25 implements ContestSubmission
 	}
 	
 	public static void main(String[] args){
-		System.out.print("Hello World!");
+		System.out.print("Hello W!");
 	}
 	
 	public void setSeed(long seed)
