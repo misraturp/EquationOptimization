38b8991bcbaea902d83c2cea886eee6657b93cfa
AnneJonker <31958557+AnneJonker@users.noreply.github.com>
2017-09-18 14:07:09 +0200

    My changes are better then yours keep mine

--- src/player25.java
+++ src/player25.java
@@ -16,7 +16,7 @@ public class player25 implements ContestSubmission
 	}
 	
 	public static void main(String[] args){
-		System.out.print("skjfndsk");
+		System.out.print("SO FED UP WITH THIS");
 	}
 	
 	public void setSeed(long seed)
