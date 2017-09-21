29a938215a3c8d5c07b69810b626f4fb6bf003b5
misra <misra@145.108.64.111>
2017-09-21 17:51:43 +0200

    simple cross over added

--- src/player25.java
+++ src/player25.java
@@ -18,9 +18,7 @@ public class player25 implements ContestSubmission
 	}
 	
 	public static void main(String[] args){
-		double [] array = {1.4,2,4,7.3,1.6};
-		Arrays.sort(array);
-		System.out.print("didsjfjf");
+		System.out.print("Done!");
 	}
 	
 	public void setSeed(long seed)
@@ -114,9 +112,25 @@ public class player25 implements ContestSubmission
 		return parents;
 	}
 	
-	public void cross_over(int point_num, double parent, double [][] children)
+	public double[][] cross_over(int point_num, double[][] parents)
 	{
+		int dim =  parents[0].length;
+		double[][] children = new double[2][dim];
+		
+		int cross_point = dim/2;
+		
+		for(int i=0; i<dim; i++) {
+			if(i<cross_point) {
+				children[0][i] = parents[0][i];
+				children[1][i] = parents[1][i];
+			}
+			else {
+				children[0][i] = parents[1][i];
+				children[1][i] = parents[0][i];	
+			}
+		}
 			
+		return children;
 	}
     
 	public void run()
@@ -134,15 +148,24 @@ public class player25 implements ContestSubmission
         // calculate fitness
         while(evals<evaluations_limit_){
             // Select parents
+        	System.out.print("helo");
         	double [][] parents = selection(population, parent_number);
         	
+        	double [][]children = cross_over(1,parents);
             // Apply crossover / mutation operators
-        		
-        	
-            double child[] = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+        		        	
+            //double child[] = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+        	double child[] = children[0];
             // Check fitness of unknown function
             Double fitness = (double) evaluation_.evaluate(child);
             evals++;
+            
+            double [][] population_wChildren = Arrays.copyOf(population, size+2);
+            population_wChildren[size] = children[0];
+            population_wChildren[size+1] = children[1];
+            
+            population = selection(population_wChildren, size);
+            
             // Select survivors
             	//log fitness values with stats****///
         }
