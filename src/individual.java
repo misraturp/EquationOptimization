84470ec8c42927dd770ebc6e8a4b654b21aade95
misra <misra@DESKTOP-0U8K5CR>
2017-10-04 22:01:42 +0200

    new class b*tchees, tiny rick \m/

---
+++ src/individual.java
@@ -0,0 +1,23 @@
+
+public class individual {
+
+	double [] content = new double[10];
+	double fitness_value;
+	double comp_fit;
+	int dim = 10;
+	double probability;
+	int wins=0;
+
+	public individual() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public individual(double[] content, double fitness_value) {
+		super();
+		this.content = content;
+		this.fitness_value = fitness_value;
+		this.comp_fit = fitness_value;
+	}
+
+}
